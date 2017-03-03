9cf815689c2e35fd71c5d6a9f2bee4ea632a271f
mihass <mihass.zh@gmail.com>
2017-03-03 10:34:26 +0300

    added Occupations

---
+++ source/MedicalRepresentative.DataAccessLayer/Entities/Occupation.cs
@@ -0,0 +1,8 @@
+﻿namespace MedicalRepresentative.DataAccessLayer.Entities
+{
+    public class Occupation
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
