dd596f6ac73d0c32d3314a477bfd7e187226f5b4
mihass <mihass.zh@gmail.com>
2017-03-03 10:34:26 +0300

    added Occupations

--- source/MedicalRepresentative.DataAccessLayer/MRDbContext.cs
+++ source/MedicalRepresentative.DataAccessLayer/MRDbContext.cs
@@ -6,6 +6,7 @@ namespace MedicalRepresentative.DataAccessLayer
     public class MRDbContext: DbContext
     {
         public DbSet<Institute> Institutes { get; set; }
+        public DbSet<Occupation> Occupations { get; set; } 
 
         public MRDbContext() : base("MRConnectionString") { }
 
@@ -15,7 +16,9 @@ namespace MedicalRepresentative.DataAccessLayer
                 .Property(x => x.Name)
                 .HasMaxLength(255);
 
-
+            modelBuilder.Entity<Occupation>()
+                .Property(x => x.Name)
+                .HasMaxLength(255);
         }
     }
 }
