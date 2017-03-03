e1c01514d5ca448d1da0f6ec3819e5df78ebbc07
mihass <mihass.zh@gmail.com>
2017-03-03 10:19:16 +0300

    added DbContext, Institute entity

---
+++ source/MedicalRepresentative.DataAccessLayer/MRDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System.Data.Entity;
+using MedicalRepresentative.DataAccessLayer.Entities;
+
+namespace MedicalRepresentative.DataAccessLayer
+{
+    public class MRDbContext: DbContext
+    {
+        public DbSet<Institute> Institutes { get; set; }
+
+        public MRDbContext() : base("MRConnectionString") { }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Institute>()
+                .Property(x => x.Name)
+                .HasMaxLength(255);
+
+
+        }
+    }
+}
