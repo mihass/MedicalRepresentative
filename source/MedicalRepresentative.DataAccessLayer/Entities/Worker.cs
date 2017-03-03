1967073e55143e9e1533f8aa8824db80662e4572
mihass <mihass.zh@gmail.com>
2017-03-03 13:18:23 +0300

    Set not null to entities names and foreign keys

--- source/MedicalRepresentative.DataAccessLayer/Entities/Worker.cs
+++ source/MedicalRepresentative.DataAccessLayer/Entities/Worker.cs
@@ -4,9 +4,9 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public int? InstituteId { get; set; }
+        public int InstituteId { get; set; }
         public virtual Institute Institute { get; set; }
-        public int? OccupationId { get; set; }
+        public int OccupationId { get; set; }
         public virtual Occupation Occupation { get; set; }
     }
 }
